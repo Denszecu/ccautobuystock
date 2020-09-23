9507c647e5c2dd6f8a65b896735289183b76c90f
Denszecu <71793328+Denszecu@users.noreply.github.com>
2020-09-23 23:50:05 +0200

    Update buy.js

--- buy.js
+++ buy.js
@@ -4,6 +4,6 @@ var value = document.getElementById("bankGood-0-val");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
-   javascript:alert($value);
+   javascript:alert('$value');
   }
 });
